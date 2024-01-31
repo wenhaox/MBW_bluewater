e85206000045615c8e5532d213a871293cd5cec4
wenhaox <42383570+wenhaox@users.noreply.github.com>
2024-01-31 16:03:17 -0500

    Added side by side graph

--- main.py
+++ main.py
@@ -5,32 +5,55 @@ import pandas as pd
 from dash import html, dcc, Dash
 from dash.dependencies import Input, Output
 import plotly.express as px
+import dash
+import dash_bootstrap_components as dbc
 
 from lib import load_data
 
-
-app = Dash(__name__)
-
-app.layout = html.Div([
-    html.H1("Water Quality Data Visualization"),
-    dcc.Graph(id='map-graph'),
-    dcc.Graph(id='nitrogen-graph'),
-    dcc.Graph(id='phosphorus-graph'),
-    dcc.Graph(id='chlorophyll-graph'),
-    dcc.Graph(id='kjeldahl-nitrogen-graph'),
-    dcc.Graph(id='enterococcus-a2la-graph'),
-    dcc.Graph(id='enterococcus-bwb-graph'),
-    dcc.Graph(id='secchi-depth-graph')
-])
+app = dash.Dash()
 
 df = load_data('./data/BWB_AWQMP_Data_2009-2022.xlsx', 'BWB Tidal Data 2009-2022')
+df['collection_date'] = pd.to_datetime(df['collection_date']) 
 
+app.layout = html.Div([
+    html.H1("Water Quality Data Visualization", style={'font-family': 'Roboto, sans-serif', 'color': '#003366'}),
+
+    html.Div([
+        html.Div([
+            dcc.Graph(id='map-graph', style={'height': '90vh'}),  
+            html.Button('Play', id='play-button', n_clicks=0),
+            html.Button('Pause', id='pause-button', n_clicks=0),
+            dcc.Interval(id='interval-component', interval=1*1000, disabled=True),
+            dcc.Store(id='date-index', data=0), 
+
+        ], style={'width': '50%', 'display': 'inline-block', 'height': '90vh'}),  
+        
+       
+        html.Div([
+            dcc.Dropdown(
+                id='graph-type-dropdown',
+                options=[
+                    {'label': 'Total Nitrogen', 'value': 'nitrogen'},
+                    {'label': 'Total Phosphorus', 'value': 'phosphorus'},
+                    {'label': 'Chlorophyll', 'value': 'chlorophyll'},
+                    {'label': 'Total Kjeldahl Nitrogen', 'value': 'kjeldahl_nitrogen'},
+                    {'label': 'Enterococcus A2LA', 'value': 'enterococcus_a2la'},
+                    {'label': 'Enterococcus BWB', 'value': 'enterococcus_bwb'},
+                    {'label': 'Secchi Depth', 'value': 'secchi_depth'}
+                ],
+                value='nitrogen',
+                style={'font-family': 'Roboto, sans-serif'}
+            ),
+            dcc.Graph(id='main-graph', style={'height': '85vh'}),  
+        ], style={'width': '50%', 'display': 'inline-block', 'vertical-align': 'top', 'height': '90vh', 'font-family': 'Roboto, sans-serif', 'color': '#005662', 'background-color': '#e6fff5'}),  
+    ], style={'height': '90vh', 'font-family': 'Roboto, sans-serif'})
+])
 
 @app.callback(
     Output('map-graph', 'figure'),
-    [Input('nitrogen-graph', 'hoverData')]
+    [Input('graph-type-dropdown', 'value')]
 )
-def update_map(hoverData):
+def update_map(graph_type):
     fig_map = px.scatter_mapbox(df,
                                 lat='latitude',
                                 lon='longitude',
@@ -42,81 +65,23 @@ def update_map(hoverData):
     return fig_map
 
 @app.callback(
-    Output('nitrogen-graph', 'figure'),
-    [Input('map-graph', 'clickData')]
+    Output('main-graph', 'figure'),
+    [Input('graph-type-dropdown', 'value')]
 )
-def update_nitrogen_graph(clickData):
-    fig_nitrogen = px.line(df,
-                           x='collection_date',
-                           y='Total Nitrogen (mg/L)',
-                           title='Total Nitrogen Over Time')
-    return fig_nitrogen
+def update_graph(graph_type):
+    y_axis = {
+        'nitrogen': 'Total Nitrogen (mg/L)',
+        'phosphorus': 'Total Phosphorus (mg/L)',
+        'chlorophyll': 'Chlorophyll (µg/L)',
+        'kjeldahl_nitrogen': 'Total Kjeldahl Nitrogen (mg/L)',
+        'enterococcus_a2la': 'Enterococcus Bacteria (MPN/100mL) - A2LA Lab',
+        'enterococcus_bwb': 'Enterococcus Bacteria (MPN/100mL) - BWB Lab',
+        'secchi_depth': 'Secchi Depth (m)'
+    }.get(graph_type, 'Total Nitrogen (mg/L)')
 
-@app.callback(
-    Output('phosphorus-graph', 'figure'),
-    [Input('map-graph', 'clickData')]
-)
-def update_phosphorus_graph(clickData):
-    fig_phosphorus = px.line(df,
-                             x='collection_date',
-                             y='Total Phosphorus (mg/L)',
-                             title='Total Phosphorus Over Time')
-    return fig_phosphorus
+    title = f"{y_axis.split('(')[0]}Over Time"
 
-@app.callback(
-    Output('chlorophyll-graph', 'figure'),
-    [Input('map-graph', 'clickData')]
-)
-def update_chlorophyll_graph(clickData):
-    fig_chlorophyll = px.line(df,
-                              x='collection_date',
-                              y='Chlorophyll (µg/L)',
-                              title='Chlorophyll Over Time')
-    return fig_chlorophyll
-
-@app.callback(
-    Output('kjeldahl-nitrogen-graph', 'figure'),
-    [Input('map-graph', 'clickData')]
-)
-def update_kjeldahl_nitrogen_graph(clickData):
-    fig_kjeldahl_nitrogen = px.line(df,
-                                    x='collection_date',
-                                    y='Total Kjeldahl Nitrogen (mg/L)',
-                                    title='Total Kjeldahl Nitrogen Over Time')
-    return fig_kjeldahl_nitrogen
-
-@app.callback(
-    Output('enterococcus-a2la-graph', 'figure'),
-    [Input('map-graph', 'clickData')]
-)
-def update_enterococcus_a2la_graph(clickData):
-    fig_enterococcus_a2la = px.line(df,
-                                    x='collection_date',
-                                    y='Enterococcus Bacteria (MPN/100mL) - A2LA Lab',
-                                    title='Enterococcus Bacteria (A2LA Lab) Over Time')
-    return fig_enterococcus_a2la
-
-@app.callback(
-    Output('enterococcus-bwb-graph', 'figure'),
-    [Input('map-graph', 'clickData')]
-)
-def update_enterococcus_bwb_graph(clickData):
-    fig_enterococcus_bwb = px.line(df,
-                                   x='collection_date',
-                                   y='Enterococcus Bacteria (MPN/100mL) - BWB Lab',
-                                   title='Enterococcus Bacteria (BWB Lab) Over Time')
-    return fig_enterococcus_bwb
-
-@app.callback(
-    Output('secchi-depth-graph', 'figure'),
-    [Input('map-graph', 'clickData')]
-)
-def update_secchi_depth_graph(clickData):
-    fig_secchi_depth = px.line(df,
-                               x='collection_date',
-                               y='Secchi Depth (m)',
-                               title='Secchi Depth Over Time')
-    return fig_secchi_depth
+    return px.line(df, x='collection_date', y=y_axis, title=title)
 
 def parse_args():
     parser = argparse.ArgumentParser()
